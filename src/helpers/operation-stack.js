0a33780580df583e76d53519953c37779dc9880f
Markus Nordangård <nordangaard@gmail.com>
2017-04-07 15:42:27 +0300

    Initial commit on thread-manager functionality

--- src/helpers/operation-stack.js
+++ src/helpers/operation-stack.js
@@ -1,75 +1,170 @@
 import R from 'ramda';
+import { Observable, Subject } from 'rxjs';
 
-const createObjectUrl = R.memoize((src) => {
+const EventStartProcessing = 'startProcessing';
+const EventDoneProcessing = 'doneProcessing';
+const EventDataError = 'error';
+const EventDataSuccess = 'data';
+const EventFreeThread = 'freeThread';
+
+const createObjectUrl = (src) => {
   const blob = new Blob([src], { type: 'text/javascript' });
   const url = URL.createObjectURL(blob);
   return url;
-});
+};
 
-class OperationStack {
-  constructor({ maxWorkers = (window.navigator.hardwareConcurrency || 4) } = {}) {
-    this.maxWorkers = maxWorkers;
-    this.activeWorkers = 0;
-    this.stack = [];
+class EventManager extends Subject {
+  constructor(init) {
+    super(init);
   }
 
-  get hasFreeWorkers() {
-    return this.activeWorkers !== this.maxWorkers;
+  on(eventType, fn) {
+    return this.filter(val => val).filter(({ type }) => type === eventType).subscribe(fn);
   }
 
-  get stackEmpty() {
-    return this.stack.length === 0;
+  one(eventType, fn) {
+    const subscription = this.filter(val => val).filter(({ type }) => type === eventType).subscribe((data) => {
+      fn(data);
+      subscription.unsubscribe();
+    });
+    return subscription;
   }
 
-  spawnWorker(src) {
-    const url = createObjectUrl(src);
-    const worker = new Worker(url);
+  streamFilter(stream, eventType) {
+    return stream.filter(({ type }) => type === eventType);
+  }
+}
 
-    return this.activeWorkers++, worker;
+class ThreadManager extends EventManager {
+  constructor({ maxThreads = (window.navigator.hardwareConcurrency || 4) } = {}) {
+    super(null);
+    this.maxThreads = maxThreads;
+    this.threads = Array.apply(null, { length: this.maxThreads }).map(() => new Thread());
+    this.activeThreads = 0;
+
+    const stream = Observable.merge.apply(this, this.threads).filter(val => val);
+    this.threadEvents(stream);
   }
 
-  destroyWorker(worker) {
-    return worker.terminate(), this.activeWorkers--, this;
+  threadEvents(stream) {
+    stream.subscribe((event) => {
+    //console.log(event, this.activeThreads);
+      switch(event.type) {
+        case(EventStartProcessing):
+          this.activeThreads++;
+        break;
+        case(EventDoneProcessing):
+          this.activeThreads--;
+          this.next({ type: EventFreeThread, thread: event.thread });
+        break;
+        case(EventDataSuccess):
+        case(EventDataError):
+          this.next(event);
+        break;
+      }
+    });
   }
 
-  queue(operation) {
-    this.stack.push(operation);
-    this.next();
+  requestThread() {
+    if (this.hasFreeThreads) {
+      const thread = this.threads.filter(thread => !thread.active).pop();
+      this.next({ type: EventFreeThread, thread });
+    }
   }
 
-  batch(operation){
-    this.stack.push(operation);
-    this.next();
+  get hasFreeThreads() {
+    return (this.activeThreads !== this.maxThreads);
   }
 
-  next() {
-    if (this.hasFreeWorkers && !this.stackEmpty) {
-      this.process(this.stack.pop());
+}
+
+class Thread extends EventManager {
+  constructor() {
+    super(null);
+    this.currentWorker = null;
+    this.jobId = null;
+    this.active = false;
+
+    this.on(EventStartProcessing, event => this.active = true);
+    this.on(EventDoneProcessing, event => this.active = false);
+  }
+
+  process(operation, bool) {
+    this.next({ type: EventStartProcessing });
+
+    let worker;
+    if (bool || operation.id === this.jobId) {
+      worker = this.currentWorker;
+    } else {
+      worker = this.spawnWorker(operation.src);
     }
 
-    return this;
-  }
-
-  process({ src, data, promise }) {
-    const worker = this.spawnWorker(src);
-    const [resolve, reject] = promise;
-
     worker.onmessage = ({ data }) => {
-      this.destroyWorker(worker);
-      this.next();
-
-      resolve(data);
+      this.next({ type: EventDataSuccess, operation, data });
+      this.next({ type: EventDoneProcessing, thread: this });
     };
 
     worker.onerror = (e) => {
-      this.destroyWorker(worker);
-      this.next();
-
-      reject(e);
+      this.next({ type: EventDataError, operation, e });
+      this.next({ type: EventDoneProcessing, thread: this });
     };
 
-    worker.postMessage(data);
+    worker.postMessage(operation.data);
   }
+
+  spawnWorker(src) {
+    if (this.currentWorker) {
+      this.currentWorker.terminate();
+    }
+
+    const url = createObjectUrl(src);
+    const worker = new Worker(url);
+
+    return this.currentWorker = worker;
+  }
+}
+
+class OperationStack {
+  constructor() {
+    this.threadManager = new ThreadManager();
+    this.stack = [];
+
+    this.threadManager.on('data', ({ operation, data }) => {
+      //console.log('Data', operation);
+      const [resolve] = operation.promise;
+      resolve(data);
+    });
+
+    this.threadManager.on('error', ({ operation, e }) => {
+      //console.log('Error');
+      const [_, reject] = operation.promise;
+      reject(e);
+    });
+
+    this.threadManager.on('freeThread', ({ thread }) => {
+      //console.log('Free thread');
+      if (this.stack.length > 0) {
+        thread.process(this.stack.shift());
+      }
+    });
+  }
+
+  addOperation(operation) {
+    this.stack.push(operation);
+
+    if (this.stack.length > 0) {
+      this.threadManager.requestThread();
+    }
+  }
+
+  queue(operation) {
+    this.addOperation(operation);
+  }
+
+  batch() {
+
+  }
+
 }
 
 export default OperationStack;
